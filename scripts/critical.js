d632526ec622f44c212d497d8a485b766fbbdfd5
Clara Beyer <csb324@gmail.com>
2016-07-04 22:54:41 -0400

    Refactoring critical css

--- scripts/critical.js
+++ scripts/critical.js
@@ -1,6 +1,5 @@
 var fs = require('hexo-fs');
 
 hexo.locals.set('critical', function(){
-	console.log("DOING CRITICAL CSS");
 	return fs.readFileSync('themes/theme2/source/css/critical.min.css');
 });
