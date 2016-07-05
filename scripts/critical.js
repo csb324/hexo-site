e65e47ed54e10df003e04aa59cb73ba0c87373d1
Clara Beyer <csb324@gmail.com>
2016-07-04 22:02:08 -0400

    Hmm... trying critical CSS again

--- scripts/critical.js
+++ scripts/critical.js
@@ -1,5 +1,6 @@
 var fs = require('hexo-fs');
 
 hexo.locals.set('critical', function(){
+	console.log("DOING CRITICAL CSS");
 	return fs.readFileSync('themes/theme2/source/css/critical.min.css');
 });
